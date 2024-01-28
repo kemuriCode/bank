e2664fe2b76d16b2516a3b4f70f24909542e3ae1
Kemuri Codes <contact@kemuri.codes>
2024-01-28 13:54:12 +0100

    Dodanie dependencies i endpoints do AccountController

--- bank/src/main/java/com/bank/bank/controller/AccountController.java
+++ bank/src/main/java/com/bank/bank/controller/AccountController.java
@@ -1,5 +1,40 @@
 package com.bank.bank.controller;
 
+import com.bank.bank.model.Account;
+import com.bank.bank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts") // Podstawowa ścieżka dla wszystkich endpointów związanych z kontami
 public class AccountController {
-    
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        Account newAccount = accountService.createAccount(account);
+        return ResponseEntity.ok(newAccount);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccount(@PathVariable Long id) {
+        Account account = accountService.getAccount(id);
+        return ResponseEntity.ok(account);
+    }
+
+    @GetMapping("/balance-greater-than/{balance}")
+    public ResponseEntity<List<Account>> getAccountsByBalanceGreaterThan(@PathVariable BigDecimal balance) {
+        List<Account> accounts = accountService.getAccountsByBalanceGreaterThan(balance);
+        return ResponseEntity.ok(accounts);
+    }
 }
